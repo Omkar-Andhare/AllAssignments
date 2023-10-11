fe180eafd9808afa5d7c5220dbedb888f37855b3
Omkar Andhare <omkar.andhare@perennial.com>
2023-10-11 17:17:12 +0530

    array code practiced

---
+++ Assignment_Omkar/src/DataStructure/ArrayPractices.java
@@ -0,0 +1,57 @@
+package DataStructure;
+
+import java.util.Scanner;
+
+public class ArrayPractices {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+  /*      System.out.println("size :- ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+
+        System.out.println(" Enter the array elements");// accepting array
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array is: ");//printing array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(" " + arr[i] + "\n");
+        }
+        System.out.println("Reverse array is : ");//reversing and printing array
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(" " + arr[i]);
+        }
+        System.out.println("---------------------------------------------------------");
+
+
+        String[] str = new String[3];
+        System.out.println("enter the strings");//accepting string array
+        for (int i = 0; i < str.length; i++) {
+            str[i] = sc.next();
+        }
+        System.out.println("---------------------------");
+        for (int i = 0; i < str.length; i++) {//printing string array
+            System.out.println(str[i]);
+        }
+        System.out.println("---------------------------------");
+        for (int i = str.length - 1; i >= 0; i--) {//reversing string array
+            System.out.println(str[i]);
+        }
+         */
+
+        int[] arr1 = {2, 3, 4, 1, 2};
+        int[] arrCopy = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arrCopy[i] = arr1[i];
+        }
+        for (int i = 0; i < arr1.length; i++) {//copying array from another array
+            System.out.println(arrCopy[i]);
+            ;
+        }
+
+
+    }
+}
